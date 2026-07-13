48bad293d6ebdd23015f3e371cd7232c1c08169d
AraAra064 <130837582+AraAra064@users.noreply.github.com>
2026-07-13 01:08:01 +0100

    Adjusted colour table sampler

--- BetterLessColours.hpp
+++ BetterLessColours.hpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <list>
 #include <unordered_map>
 #include <thread>
 #include <algorithm>
@@ -39,11 +40,12 @@ namespace blc
 	}
 
 	//Returns an optimized colour palette from an image
-	//Colour format does not matter (RGBA/BGRA) 
-	std::vector<uint32_t> GetColourTable(std::vector<uint32_t>& pixels, uint32_t tableSize = D_CTSIZE, float pStep = D_PSTEP)
+	//Colour format does not matter (RGBA/BGRA), returns colours in the same format as inputted
+	std::vector<uint32_t> GetColourTable(const std::vector<uint32_t>& pixels, uint32_t tableSize = D_CTSIZE, float pStep = D_PSTEP)
 	{
 		std::unordered_map<uint32_t, uint32_t> colourTable;
 		std::vector<ColourData> ct;
+		std::vector<uint32_t> table(tableSize, 0);
 		//colourTable[0x00000000] = -1;
 
 		for (uint32_t c : pixels)
@@ -61,10 +63,6 @@ namespace blc
 			}
 		}
 
-		//if (colourTable.size() <= colourTableSize) {
-		//	return;
-		//}
-
 		ct.reserve(colourTable.size());
 		for (auto c : colourTable)
 		{
@@ -72,41 +70,35 @@ namespace blc
 		}
 		std::sort(ct.begin(), ct.end(), [](ColourData& a, ColourData& b)->bool {return a.n < b.n;});
 
-		int iterations = (int)(100.f / pStep);
-		//always keep most common colour, maybe change this
-		int ctStepSize = (int)round(tableSize * (pStep / 100.f));
-		if (ctStepSize == 0)
+		//CTSampler
+		int inCTStep = ct.size() * pStep;
+		int outCTStep = tableSize * pStep;
+		float ctsRatio = (float)ct.size() / (float)tableSize;
+		int iterations = (int)std::trunc((float)tableSize / (float)outCTStep); //ceil/round??
+		for (int i = 0; i < iterations; i++)
 		{
-			ctStepSize++;
-		}
-		//????
-		for (int i = 1, pos = ctStepSize + (tableSize % ctStepSize); i < iterations && pos < ct.size(); i++)
-		{
-			memcpy(&ct[pos], &ct[(int)(ct.size() * ((float)i / (float)iterations)) - (ctStepSize / 2)], ctStepSize * sizeof(ColourData));
-			pos += ctStepSize;
-		}
+			for (int outIt = 0; outIt < outCTStep; outIt++)
+			{
+				int inPos = (i * inCTStep) + (outIt * ctsRatio);
+				int outPos = (i * outCTStep) + outIt;
 
-		ct.resize(tableSize);
-		std::vector<uint32_t> table(tableSize);
-		for (size_t i = 0; i < table.size(); i++)
-		{
-			table[i] = ct[i].c;
+				table[outPos] = ct[inPos].c;
+			}
 		}
-
-		//merge common colours to increase accuracy?
 
 		return table;
 	}
 
-	//Returns image with optimized palette
-	std::vector<uint32_t> ReduceColours(std::vector<uint32_t>& pixels, std::vector<uint32_t> colourTable)
+	//Returns image pixel data using optimized colour palette
+	//Colour format will be the same as the input
+	std::vector<uint32_t> ReduceColours(const std::vector<uint32_t>& pixels, std::vector<uint32_t> colourTable)
 	{
-		std::vector<uint32_t> optPixels(pixels.size());
+		std::vector<uint32_t> optPixels = pixels;
 		std::unordered_map<uint32_t, uint32_t> dict;
 
 		for (size_t i = 0; i < pixels.size(); i++)
 		{
-			uint32_t& pixel = pixels[i];
+			uint32_t& pixel = optPixels[i];
 
 			if (dict.count(pixel) == 0)
 			{
@@ -131,8 +123,8 @@ namespace blc
 				}
 
 				dict[pixel] = _c;
-				optPixels[i] = _c;
-			} else optPixels[i] = dict[pixel];
+				pixel = _c;
+			} else pixel = dict[pixel];
 		}
 
 		return optPixels;
